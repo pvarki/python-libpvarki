41df85fe524e18e19a05e642a416f8026c4cf67f
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-07-02 18:12:09 +0300

    test the pydantic schema roundtrip encodes

--- tests/middleware/test_product.py
+++ tests/middleware/test_product.py
@@ -4,7 +4,9 @@ import uuid
 
 import pytest
 from fastapi.testclient import TestClient
+from pydantic import ValidationError
 
+from libpvarki.schemas.product import UserCRUDRequest, OperationResultResponse, UserInstructionFragment
 from .test_middleware import mtlsclient  # pylint: disable=W0611
 
 
@@ -14,6 +16,50 @@ from .test_middleware import mtlsclient  # pylint: disable=W0611
 def create_user_dict(callsign: str) -> Dict[str, str]:
     """return valid user dict for crud operations"""
     return {"uuid": str(uuid.uuid4()), "callsign": callsign, "x509cert": "FIXME: insert dummy cert in CFSSL encoding"}
+
+
+def test_operresult_validationfail() -> None:
+    """Test that OperationResultResponse can be encoded and decoded"""
+    with pytest.raises(ValidationError):
+        _pcresp = OperationResultResponse.parse_obj(
+            {
+                "success": "dummy",
+                "error": False,
+            }
+        )
+
+
+def test_usercrud_encdec() -> None:
+    """Test that UserCRUDRequest can be encoded and decoded"""
+    pdcrud = UserCRUDRequest.parse_obj(create_user_dict("KETTU11b"))
+    encoded = pdcrud.json()
+    decoded = UserCRUDRequest.parse_raw(encoded)
+    assert decoded.uuid == pdcrud.uuid
+
+
+def test_operresult_encdec() -> None:
+    """Test that OperationResultResponse can be encoded and decoded"""
+    pcresp = OperationResultResponse.parse_obj(
+        {
+            "success": False,
+            "error": "Dummy",
+        }
+    )
+    encoded = pcresp.json()
+    decoded = OperationResultResponse.parse_raw(encoded)
+    assert decoded.error == pcresp.error
+
+
+def test_instruction_encdec() -> None:
+    """Test that UserInstructionFragment can be encoded and decoded"""
+    pdinstr = UserInstructionFragment.parse_obj(
+        {
+            "html": "<p>Hello world!</p>",
+        }
+    )
+    encoded = pdinstr.json()
+    decoded = UserInstructionFragment.parse_raw(encoded)
+    assert decoded.html == pdinstr.html
 
 
 @pytest.fixture(scope="session")
