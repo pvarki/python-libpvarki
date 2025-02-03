8deb0b3d95ace28f996d219c52e137483b508e52
Aarni Koskela <akx@iki.fi>
2025-02-03 16:45:53 +0200

    `uvx bump2version major`

--- src/libpvarki/__init__.py
+++ src/libpvarki/__init__.py
@@ -1,2 +1,2 @@
 """ Common helpers like standard logging init """
-__version__ = "1.9.1"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "2.0.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
